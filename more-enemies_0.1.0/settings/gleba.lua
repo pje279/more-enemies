52a51031cd5a6e31785563eb99871b1f8fd814dc
Peter <p.eckelmann@gmail.com>
2025-03-11 13:29:40 -0500

    0.1.0 Refactored settings.lua into per planet settings

---
+++ more-enemies_0.1.0/settings/gleba.lua
@@ -0,0 +1,44 @@
+data:extend({
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-gleba-small-max-count-of-owned-units",
+      setting_type = "startup",
+      default_value = 1,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-gleba-small-max-friends-around-to-spawn",
+      setting_type = "startup",
+      default_value = 2,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-gleba-max-count-of-owned-units",
+      setting_type = "startup",
+      default_value = 2,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-gleba-max-count-of-owned-defensive-units",
+      setting_type = "startup",
+      default_value = 1,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-gleba-max-friends-around-to-spawn",
+      setting_type = "startup",
+      default_value = 3,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-gleba-max-defensive-friends-around-to-spawn",
+      setting_type = "startup",
+      default_value = 2,
+      minimum_value = 0,
+  }
+})
---
+++ more-enemies_0.1.0/settings/nauvis.lua
@@ -0,0 +1,30 @@
+data:extend({
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-nauvis-max-count-of-owned-units-biter",
+      setting_type = "startup",
+      default_value = 7,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-nauvis-max-friends-around-to-spawn-biter",
+      setting_type = "startup",
+      default_value = 5,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-nauvis-max-count-of-owned-units-spitter",
+      setting_type = "startup",
+      default_value = 7,
+      minimum_value = 0,
+  },
+  {
+      type = "int-setting",
+      name = "more-enemies-spawner-nauvis-max-friends-around-to-spawn-spitter",
+      setting_type = "startup",
+      default_value = 5,
+      minimum_value = 0,
+  }
+})
