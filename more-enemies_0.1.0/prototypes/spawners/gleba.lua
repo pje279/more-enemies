0f1ed5358dfd67fec3fb5b85000c099229612ef3
Peter <p.eckelmann@gmail.com>
2025-03-13 17:31:34 -0500

    0.1.0 Added planet icons to settings; ordered the settings; added additional settings for Gleba

--- more-enemies_0.1.0/prototypes/spawners/gleba.lua
+++ more-enemies_0.1.0/prototypes/spawners/gleba.lua
@@ -9,9 +9,29 @@ if (max_count_of_owned_units_small and max_count_of_owned_units_small.value) the
   data.raw["unit-spawner"]["gleba-spawner-small"].max_count_of_owned_units = max_count_of_owned_units_small.value
 end
 
+local max_count_of_owned_defensive_units_small = settings.startup["more-enemies-spawner-gleba-small-max-count-of-owned-defensive-units"]
+if (max_count_of_owned_defensive_units_small and max_count_of_owned_defensive_units_small.value) then
+  data.raw["unit-spawner"]["gleba-spawner"].max_count_of_owned_defensive_units = max_count_of_owned_defensive_units_small.value
+end
+
 local max_friends_around_to_spawn_small = settings.startup["more-enemies-spawner-gleba-small-max-friends-around-to-spawn"]
 if (max_friends_around_to_spawn_small and max_friends_around_to_spawn_small.value) then
   data.raw["unit-spawner"]["gleba-spawner-small"].max_friends_around_to_spawn = max_friends_around_to_spawn_small.value
+end
+
+local max_defensive_friends_around_to_spawn_small = settings.startup["more-enemies-spawner-gleba-small-max-defensive-friends-around-to-spawn"]
+if (max_defensive_friends_around_to_spawn_small and max_defensive_friends_around_to_spawn_small.value) then
+  data.raw["unit-spawner"]["gleba-spawner"].max_defensive_friends_around_to_spawn = max_defensive_friends_around_to_spawn_small.value
+end
+
+local max_spawning_cooldown_gleba_small = settings.startup["more-enemies-max-spawning-cooldown-gleba-small"]
+local min_spawning_cooldown_gleba_small = settings.startup["more-enemies-min-spawning-cooldown-gleba-small"]
+if (    max_spawning_cooldown_gleba_small
+    and max_spawning_cooldown_gleba_small.value
+    and min_spawning_cooldown_gleba_small
+    and min_spawning_cooldown_gleba_small.value
+  ) then
+      data.raw["unit-spawner"]["gleba-spawner-small"].spawning_cooldown = {max_spawning_cooldown_gleba_small.value, min_spawning_cooldown_gleba_small.value}
 end
 
 --
@@ -44,6 +64,5 @@ if (    max_spawning_cooldown_gleba
     and min_spawning_cooldown_gleba
     and min_spawning_cooldown_gleba.value
   ) then
-      data.raw["unit-spawner"]["gleba-spawner-small"].spawning_cooldown = {max_spawning_cooldown_gleba.value, min_spawning_cooldown_gleba.value}
       data.raw["unit-spawner"]["gleba-spawner"].spawning_cooldown = {max_spawning_cooldown_gleba.value, min_spawning_cooldown_gleba.value}
 end
