3525c5b997fec7f87cdbf71a5153f53e7623e83a
Peter <p.eckelmann@gmail.com>
2025-03-21 07:50:21 -0500

    0.3.x Fixed main menu simulation error when difficulty is turned up

--- more-enemies_0.3.0/data.lua
+++ more-enemies_0.3.0/data.lua
@@ -4,3 +4,6 @@ require("prototypes.spawners.nauvis")
 if (mods["space-age"]) then
   require("prototypes.spawners.gleba")
 end
+
+-- Fix the character dying in menu_simulations.nauvis_biter_base_laser_defense when difficulty is turned up
+require("menu-simulations.menu-simulations")
