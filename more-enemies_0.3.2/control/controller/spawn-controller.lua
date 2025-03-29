4288b7a598910a369e678ee6aa02588746d02288
Peter <p.eckelmann@gmail.com>
2025-03-29 16:32:40 -0500

    bug-fix/0.3.x Additional checks

--- more-enemies_0.3.2/control/controller/spawn-controller.lua
+++ more-enemies_0.3.2/control/controller/spawn-controller.lua
@@ -27,7 +27,8 @@ for k,v in pairs(Gleba_Constants.gleba.categories) do
   table.insert(spawn_controller.filter, { filter = "name", name = v .. "-strafer-pentapod"})
   table.insert(spawn_controller.filter, { filter = "name", name = v .. "-stomper-pentapod"})
 end
-if ((mods and mods["behemoth-enemies"]) or (script and script.active_mods and script.active_mods["behemoth-enemies"])) then
+
+if ((mods and mods["space-age"] and mods["behemoth-enemies"]) or (script and script.active_mods and script.active_mods["space-age"] and script.active_mods["behemoth-enemies"])) then
   table.insert(spawn_controller.filter, { filter = "name", name = Behemoth_Enemies_Constants.prefix .. "-wriggler-pentapod" })
   table.insert(spawn_controller.filter, { filter = "name", name = Behemoth_Enemies_Constants.prefix .. "-strafer-pentapod"})
   table.insert(spawn_controller.filter, { filter = "name", name = Behemoth_Enemies_Constants.prefix .. "-stomper-pentapod"})
