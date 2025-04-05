ca31306b6e844a150de9a8343987ac4c6936d958
Peter <p.eckelmann@gmail.com>
2025-04-04 20:42:52 -0500

    feature/add-use-bream-toggle Updated changelog

--- more-enemies_0.5.1/libs/constants/mods/BREAM-constants.lua
+++ more-enemies_0.5.1/libs/constants/mods/BREAM-constants.lua
@@ -6,9 +6,9 @@ end
 local BREAM_constants = {}
 
 BREAM_constants.name = "BREAM"
+BREAM_constants.do_clone = false
 
 BREAM_constants.more_enemies = true
-BREAM_constants.do_clone = false
 
 local _BREAM_constants = BREAM_constants
 
