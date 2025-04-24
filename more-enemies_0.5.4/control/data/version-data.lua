758e9beec0062eba2e2560025bf3d0b09af92d66
Peter <p.eckelmann@gmail.com>
2025-04-24 14:01:44 -0500

    bug-fix/nil-difficulty Initial commit

--- more-enemies_0.5.4/control/data/version-data.lua
+++ more-enemies_0.5.4/control/data/version-data.lua
@@ -13,7 +13,7 @@ version_data.minor = Minor_Data:new()
 version_data.minor.value = 5
 version_data.minor.valid = true
 version_data.bug_fix = Bug_Fix_Data:new()
-version_data.bug_fix.value = 3
+version_data.bug_fix.value = 4
 version_data.bug_fix.valid = true
 
 version_data.string_val = version_data.major.value .. "." .. version_data.minor.value .. "." .. version_data.bug_fix.value
